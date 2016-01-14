d6ca38498b1405955e4d5c2ef396419bdc588bdd
Jaden <m0de@null.net>
2016-01-14 06:26:44 -0500

    Recent File Updates
    Commit for DevBest Thread.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var logger = require('./lib/logger'),
     //redis = require('redis'),
     //client = redis.createClient(),
+    Database = require('./lib/Database').database,
+    database,
     Network = require('./lib/Network'),
     network;
+
+database = new Database();
 
 network = new Network(3001);
 network.invokeTcpServer();
---
+++ config.js
@@ -0,0 +1,8 @@
+var config = [];
+
+config.host = "localhost";
+config.user = "root";
+config.pass = "";
+config.database = "mango_db";
+
+module.exports = config;
